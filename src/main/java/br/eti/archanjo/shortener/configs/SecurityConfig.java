cfbd9ada2225a03789f2387127eb321da3896782
Fabricio Archanjo <farchanjo@gmail.com>
2017-08-06 04:33:20 -0300

    First security impl.

---
+++ src/main/java/br/eti/archanjo/shortener/configs/SecurityConfig.java
@@ -0,0 +1,49 @@
+package br.eti.archanjo.shortener.configs;
+
+import br.eti.archanjo.shortener.constants.ExceptionConstants;
+import br.eti.archanjo.shortener.constants.PathContants;
+import br.eti.archanjo.shortener.filters.CustomAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .failureHandler(
+                        (request, response, exception) -> response.sendError(400, ExceptionConstants.PASSWORD_DOES_NOT_MATCH)
+                )
+                .successHandler(
+                        (request, response, authentication) -> response.sendError(200, "logged")
+                )
+                .loginPage(PathContants.LOGIN).passwordParameter("password").usernameParameter("emailorusername").permitAll()
+                .and()
+                .logout().logoutUrl(PathContants.LOGOUT).permitAll()
+                .invalidateHttpSession(true)
+                .logoutSuccessHandler(
+                        (request, response, authentication) -> response.sendError(200, "logout ok")
+                )
+                .and()
+                .exceptionHandling().accessDeniedHandler(customAuthenticationEntryPoint).authenticationEntryPoint(customAuthenticationEntryPoint)
+                .and()
+                .headers().xssProtection().block(true)
+                .and()
+                .frameOptions().sameOrigin()
+                .and()
+                .csrf().disable();
+    }
+
+
+}
---
+++ src/main/java/br/eti/archanjo/shortener/filters/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,40 @@
+package br.eti.archanjo.shortener.filters;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint, AccessDeniedHandler {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException,
+            ServletException {
+        if (request.getAttribute("RESTEASY_CHOSEN_ACCEPT") == null) {
+            response.setContentType("application/json;charset=UTF8");
+        } else {
+            response.setContentType(request.getAttribute("RESTEASY_CHOSEN_ACCEPT").toString());
+        }
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        PrintWriter out = response.getWriter();
+        out.println("{\"response\": \"ACCESS_DENIED\"}");
+    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
+            throws IOException, ServletException {
+        this.commence(request, response, null);
+    }
+}
