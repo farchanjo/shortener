9109e5c1a5975b25ab5976ad2e51f76db9796a60
Fabricio Archanjo <farchanjo@gmail.com>
2017-08-06 04:37:13 -0300

    Command line runner added.

--- src/main/java/br/eti/archanjo/shortener/configs/main/ShortenerApplication.java
+++ src/main/java/br/eti/archanjo/shortener/configs/main/ShortenerApplication.java
@@ -1,12 +1,18 @@
 package br.eti.archanjo.shortener.configs.main;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "br.eti.archanjo.shortener")
-public class ShortenerApplication {
+@SpringBootApplication(scanBasePackages = "br.eti.archanjo")
+public class ShortenerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShortenerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
