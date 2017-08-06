7ab51b27661f52083b1f576f1e38eaf3866e83c1
Fabricio Archanjo <farchanjo@gmail.com>
2017-08-06 04:42:14 -0300

    Change somes params on Spring Security.

--- src/main/java/br/eti/archanjo/shortener/configs/SecurityConfig.java
+++ src/main/java/br/eti/archanjo/shortener/configs/SecurityConfig.java
@@ -5,6 +5,7 @@ import br.eti.archanjo.shortener.constants.PathContants;
 import br.eti.archanjo.shortener.filters.CustomAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,7 +20,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().permitAll()
+                .antMatchers("/*").permitAll()
+                .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin()
                 .failureHandler(
@@ -45,5 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable();
     }
 
-
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(provider);
+    }
 }
