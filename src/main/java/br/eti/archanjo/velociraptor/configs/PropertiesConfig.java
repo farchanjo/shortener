fecc83c5df581fe31212a86569eb46a9f8ac9936
Fabricio Archanjo Fonseca <farchanjo@gmail.com>
2017-08-12 16:33:55 -0300

    Default Redirect.

--- src/main/java/br/eti/archanjo/velociraptor/configs/PropertiesConfig.java
+++ src/main/java/br/eti/archanjo/velociraptor/configs/PropertiesConfig.java
@@ -4,6 +4,8 @@ import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
+
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Getter
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "velociraptor")
 public class PropertiesConfig {
     private HttpLimits httplimits;
+    private URI defaultRedirect;
 
     @AllArgsConstructor(access = AccessLevel.PUBLIC)
     @NoArgsConstructor(access = AccessLevel.PUBLIC)
