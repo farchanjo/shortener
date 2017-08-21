a9ece8163876b2477006c714cdb95b3337ee92a5
Fabricio Archanjo Fonseca <farchanjo@gmail.com>
2017-08-21 18:30:09 -0300

    Comment.

--- src/main/java/br/eti/archanjo/velociraptor/configs/Schedulers.java
+++ src/main/java/br/eti/archanjo/velociraptor/configs/Schedulers.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 public class Schedulers {
@@ -17,22 +16,22 @@ public class Schedulers {
     public Schedulers(RequestRepository requestRepository) {
         this.requestRepository = requestRepository;
     }
-
-    @Scheduled(fixedDelay = 5000L, initialDelay = 10000L)
-    private void consolidateData() {
-        logger.info("Started");
-/*        Page<RequestEntity> entities = requestRepository.findAllByOrderByCreatedDesc(new PageRequest(0, 1000));
-        int pageCounter = 0;
-        while (pageCounter < entities.getTotalPages()) {
-            entities.forEach(p -> {
-
-            });
-            *//*
-             * Getting new page
-             *//*
-            entities = requestRepository.findAllByOrderByCreatedDesc(new PageRequest(0, 1000));
-            pageCounter++;
-        }*/
-        logger.info("finished");
-    }
+//
+//    @Scheduled(fixedDelay = 5000L, initialDelay = 10000L)
+//    private void consolidateData() {
+//        logger.info("Started");
+//        Page<RequestEntity> entities = requestRepository.findAllByOrderByCreatedDesc(new PageRequest(0, 1000));
+//        int pageCounter = 0;
+//        while (pageCounter < entities.getTotalPages()) {
+//            entities.forEach(p -> {
+//
+//            });
+//            *//*
+//             * Getting new page
+//             *//*
+//            entities = requestRepository.findAllByOrderByCreatedDesc(new PageRequest(0, 1000));
+//            pageCounter++;
+//        }
+//        logger.info("finished");
+//    }
 }
